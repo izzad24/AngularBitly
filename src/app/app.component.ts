32aa92127183d89a690299a82c03f2eb7204a1f1
izzad24 <afifizzad_mazlan@dell.com>
2019-09-22 10:55:49 +0800

    complete front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+
+export interface Data{
+  shortUrl: string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBitly';
+  inputUrl = new FormControl('')
+  outputUrl = new FormControl('')
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+  }
+  onClick(){
+    let url = "https://warm-scrubland-03694.herokuapp.com/urls"
+    let params = {
+      "urls": this.inputUrl.value,
+    }
+
+    console.log(params)
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    }
+
+    this.http.post(url, params, httpOptions ).subscribe((data: Data) => {
+      console.log(data.shortUrl)
+       this.outputUrl.setValue(data.shortUrl)
+    })
+  }
 }
